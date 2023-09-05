f86d34f8dd615803de5cce6db1d29abcaa26bd7c
maximeflageole <maximeflageole@gmail.com>
2023-09-05 15:52:47 -0500

    Adding an IState interface Adding a CharacterControllerStateMachine Adding a FreeState to control the player character

---
+++ TP_Engin1/Assets/Scripts/CharacterStateMachine/CharacterState.cs
@@ -0,0 +1,25 @@
+public class CharacterState : IState
+{
+    protected CharacterControllerStateMachine m_stateMachine;
+
+    public void OnStart(CharacterControllerStateMachine stateMachine)
+    {
+        m_stateMachine = stateMachine;
+    }
+
+    public virtual void OnEnter()
+    {
+    }
+
+    public virtual void OnExit()
+    {
+    }
+
+    public virtual void OnFixedUpdate()
+    {
+    }
+
+    public virtual void OnUpdate()
+    {
+    }
+}
